dbdd6d035049b21e4741b1b2609d957b94d37197
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2024-04-03 12:03:26 +0530

    Competition.java

--- src/test/java/com/test/Code4humanity/Competition.java
+++ src/test/java/com/test/Code4humanity/Competition.java
@@ -28,7 +28,7 @@ public class Competition {
 			{
 				
 				  DesiredCapabilities dc = DesiredCapabilities.chrome();
-			        URL url = new URL("http://172.20.23.7:5555/wd/hub");
+			        URL url = new URL("http://172.12.20.99:4444/wd/hub");
 			        driver = new RemoteWebDriver(url, dc);
 			
 			       
@@ -79,7 +79,7 @@ public class Competition {
 			        
 			        WebElement emailInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='email']")));
 			        if (emailInput != null && emailInput.isDisplayed()) {
-			            emailInput.sendKeys("teamsoftware457@gmail.com");
+			            emailInput.sendKeys("softwareteam45@gmail.com");
 			            System.out.println("Email was entered successfully.");
 			        } else {
 			           System.out.println("Email was not entered.");
