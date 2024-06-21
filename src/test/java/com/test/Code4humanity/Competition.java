71926edaa3eaa3a66b14aad8183a89897863b15a
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2024-06-21 09:36:30 +0530

    Competition.java

--- src/test/java/com/test/Code4humanity/Competition.java
+++ src/test/java/com/test/Code4humanity/Competition.java
@@ -28,7 +28,7 @@ public class Competition {
 			{
 				
 				  DesiredCapabilities dc = DesiredCapabilities.chrome();
-			        URL url = new URL("http://172.12.20.99:4444/wd/hub");
+			        URL url = new URL("http://172.20.23.92:4444/wd/hub");
 			        driver = new RemoteWebDriver(url, dc);
 			
 			       
